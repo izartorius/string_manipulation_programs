e2a99013496dca0904f43fc151945cdc0e2f4cce
izartorius <atreuseinzbelt@gmail.com>
2025-03-28 12:37:54 +0800

    Defined the function that will convert the characters into lowercase, and initialized an empty variable to store the lowercase string

---
+++ converts_characters_to_lowercase.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that converts all string characters
+#to lowercase without using the .lower() function
+
+#Define function to convert characters to lowercase
+def to_lowercase(string):
+    # Initialize an empty result string to store the final lowercase string
+    result = ''
