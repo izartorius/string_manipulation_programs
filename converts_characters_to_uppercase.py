3dc9c63d8c3e2ea3043c0cd419041f711b235347
izartorius <atreuseinzbelt@gmail.com>
2025-03-28 12:59:22 +0800

    added a user input for the raw string then added a print statement that returns a true or false output whether all the characters are in uppercase or not

--- converts_characters_to_uppercase.py
+++ converts_characters_to_uppercase.py
@@ -6,4 +6,8 @@ def is_all_uppercase(string):
     # Use all() to check if every letter in the string is uppercase
     return all('A' <= char <= 'Z' for char in string if char.isalpha())
 
+# Get user input (string to check)
+user_input = input("Enter a string to check if all characters are uppercase: ")
+# Print whether all characters in the string are uppercase
+print("Are all characters uppercase?", is_all_uppercase(user_input))
 
