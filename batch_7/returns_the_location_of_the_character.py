7e7a481625bb55f00c067ce2dbf08d124222fcfb
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 08:56:08 +0800

    added user input for the given input and the specified character of the end user

---
+++ batch_7/returns_the_location_of_the_character.py
@@ -0,0 +1,7 @@
+#Prog09: Create a program that returns the
+#location of the specified character without
+#the use of the index() function.
+
+# Get user input
+text = input("Enter a string: ")
+substring = input("Enter substring to find: ")
