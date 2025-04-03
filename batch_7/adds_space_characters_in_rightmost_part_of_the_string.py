806ccd2f259c0835598615a141d827ede3d45a30
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 08:36:32 +0800

    initialized user input for the given string and desired length or amount of spaces by the end user

---
+++ batch_7/adds_space_characters_in_rightmost_part_of_the_string.py
@@ -0,0 +1,7 @@
+# Prog06: Create a program that adds space
+# characters to the rightmost part of the
+# given string without using the rstrip() function
+
+# Get user input
+text = input("Enter a string: ")
+width = int(input("Enter total width: "))
