721279f38708c017d705865d1ffdbb45cc655647
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 08:48:32 +0800

    added a user input for the given string and desired character of the end user. initialized an empty variable list to store the count of the specified character

---
+++ batch_7/returns_the_frequency_of_a_certain_character.py
@@ -0,0 +1,8 @@
+#Prog08: Create a program that counts the frequency
+#of a certain character in the given input without
+#the use of the count() function.
+
+#Get user input
+text = input("Enter a string: ")
+substring = input("Enter substring to count: ")
+count = 0
