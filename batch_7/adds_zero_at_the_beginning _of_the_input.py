4159eac03175f797f9b8d3bc2366605a8b6b0998
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 08:42:34 +0800

    initialized user input for the given string and desired number of characters by the end user

---
+++ batch_7/adds_zero_at_the_beginning _of_the_input.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that adds zeroes
+#to the beginning of the string to complete
+#the number of characters specified without
+#the use of zfill() function.
+
+# Get user input
+text = input("Enter a string: ")
+width = int(input("Enter total width: "))
+
