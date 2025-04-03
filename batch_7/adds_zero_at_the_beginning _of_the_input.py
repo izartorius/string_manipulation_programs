628976a1b6b677e69db0d3cc2e3547a653715099
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 08:44:18 +0800

    assigned a variable that will calculate the zeroes to be added at the beginning of the string, then prints the input with the zeroes included

--- batch_7/adds_zero_at_the_beginning _of_the_input.py
+++ batch_7/adds_zero_at_the_beginning _of_the_input.py
@@ -6,4 +6,6 @@
 # Get user input
 text = input("Enter a string: ")
 width = int(input("Enter total width: "))
-
+# Calculate needed zeros and prepend them
+zfill_text = "0" * (width - len(text)) + text
+print("zfill() equivalent:", zfill_text)
