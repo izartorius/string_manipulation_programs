b51c1fcf040d2f5f945ddc323576d91e1ee02cc5
izartorius <atreuseinzbelt@gmail.com>
2025-04-03 09:25:28 +0800

    initialized user input for the given string and the desired character of the end user

---
+++ batch_7/returns_the_location_of_the_character_starting_from_the_right.py
@@ -0,0 +1,8 @@
+#Prog10: Create a program that returns the
+#location of the specified character starting
+#from the rightmost part of the string without
+#the use of the rindex() function.
+
+# Get user input
+text = input("Enter a string: ")
+substring = input("Enter substring to find from right: ")
